729893e14bd789d0aabf2c79b8599c020a66d017
toco <ttachibana@my.bcit.ca>
2022-09-03 11:14:33 -0700

    Declare and initialize variables

---
+++ variables/src/Variables.java
@@ -0,0 +1,10 @@
+public class Variables {
+    public static void main(String[] args) {
+        long studentNumber = 1279235L;
+        float gpa = 3.68f;
+        String name = "Toco";
+
+        System.out.printf("%s -> No. %d\n", name, studentNumber);
+        System.out.printf("GPA: %.2f", gpa);
+    }
+}
