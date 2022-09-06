2055577ff0c302cb8fdfea99f840ce2a5e5bf27d
toco <ttachibana@my.bcit.ca>
2022-09-06 16:10:50 -0700

    Implement encapsulation by using private keyword and access modifiers

--- inheritance/src/Footballer.java
+++ inheritance/src/Footballer.java
@@ -1,11 +1,29 @@
 public class Footballer extends Athlete{
 
-    String club;
-    int shirtNumber;
+    private String club;
+    private int shirtNumber;
 
     Footballer(String name, int age, String sport, String club, int shirtNumber) {
         super(name, age, sport);
+        this.setClub(club);
+        this.setShirtNumber(shirtNumber);
+    }
+
+    // getter methods
+    public String getClub() {
+        return this.club;
+    }
+
+    public int getShirtNumber() {
+        return this.shirtNumber;
+    }
+
+    // setter methods
+    protected void setClub(String club) {
         this.club = club;
+    }
+
+    protected void setShirtNumber(int shirtNumber) {
         this.shirtNumber = shirtNumber;
     }
 
